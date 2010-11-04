0a14b6181b4bf3bdc3f09a4f23985037cca3daf2
sebastian.nerger@gmail.com <sebastian.nerger@gmail.com@cf72ce6b-2be2-143d-d88e-ed02739776e2>
2010-11-04 09:10:58 +0000

    Preferences, Nature-Icons

---
+++ Core/src/biz/ritter/develop/cobol/prefs/COBOLPreferenceConstants.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2010 Sebastian Nerger
+ *
+ * Lizenziert unter der EUPL, nur Version 1.1 ("Lizenz");
+ * 
+ * Sie dürfen dieses Werk ausschließlich gemäß
+ * dieser Lizenz nutzen.
+ * Eine Kopie der Lizenz finden Sie hier:
+ *
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Sofern nicht durch anwendbare Rechtsvorschriften
+ * gefordert oder in schriftlicher Form vereinbart, wird
+ * die unter der Lizenz verbreitete Software "so wie sie
+ * ist", OHNE JEGLICHE GEWÄHRLEISTUNG ODER BEDINGUNGEN -
+ * ausdrücklich oder stillschweigend - verbreitet.
+ * Die sprachspezifischen Genehmigungen und Beschränkungen
+ * unter der Lizenz sind dem Lizenztext zu entnehmen.
+ */
+package biz.ritter.develop.cobol.prefs;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class COBOLPreferenceConstants {
+
+  /**
+   * Constant for COBOL compiler path
+   */
+	public static final String CODE_PREF_COBOL_COMPILER_PATH = "codeCompilerPath";
+
+  /**
+   * Constant for COBOL compiler executable name
+   */
+	public static final String CODE_PREF_COBOL_COMPILER_EXECUTABLE = "codeCompilerExec";
+	
+}
---
+++ Core/src/biz/ritter/develop/cobol/prefs/COBOLPreferenceInitializer.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2010 Sebastian Nerger
+ *
+ * Lizenziert unter der EUPL, nur Version 1.1 ("Lizenz");
+ * 
+ * Sie dürfen dieses Werk ausschließlich gemäß
+ * dieser Lizenz nutzen.
+ * Eine Kopie der Lizenz finden Sie hier:
+ *
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Sofern nicht durch anwendbare Rechtsvorschriften
+ * gefordert oder in schriftlicher Form vereinbart, wird
+ * die unter der Lizenz verbreitete Software "so wie sie
+ * ist", OHNE JEGLICHE GEWÄHRLEISTUNG ODER BEDINGUNGEN -
+ * ausdrücklich oder stillschweigend - verbreitet.
+ * Die sprachspezifischen Genehmigungen und Beschränkungen
+ * unter der Lizenz sind dem Lizenztext zu entnehmen.
+ */
+package biz.ritter.develop.cobol.prefs;
+
+import biz.ritter.develop.cobol.CorePlugInActivator;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+/**
+ * Class used to initialize default preference values.
+ */
+public class COBOLPreferenceInitializer extends AbstractPreferenceInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 */
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = CorePlugInActivator.getDefault().getPreferenceStore();
+		store.setDefault(COBOLPreferenceConstants.CODE_PREF_COBOL_COMPILER_PATH, "g:/MinGW/bin");
+		store.setDefault(COBOLPreferenceConstants.CODE_PREF_COBOL_COMPILER_EXECUTABLE, "cobc");
+	}
+
+}
